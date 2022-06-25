18c73b29dea0c53da17047f1bab714a30cbf4630
Khaleesi89 <93682145+Khaleesi89@users.noreply.github.com>
2022-06-25 13:40:46 -0300

    ya funciona en pagina principal

--- ingresoUsuario.js
+++ ingresoUsuario.js
@@ -1,16 +1,19 @@
 const usuario = document.getElementById("usuario")
 const password = document.getElementById("password")
 const formu = document.getElementById("formu")
+const parrafo = document.getElementById("warnings")
 
 formu.addEventListener("submit", e=>{
     e.preventDefault()
     let warnings = ""
     parrafo.innerHTML = ""
     if(password.value.length < 8){
-        warnings += "Password Inválido / Intente nuevamente <br>"
+        warnings += "Password Inválido <br>"
         entrar = true
     }
     if(entrar){
         parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado" 
     }
 })
