1bcbe15da8797491947be9f4170e4420b7139958
Khaleesi89 <93682145+Khaleesi89@users.noreply.github.com>
2022-06-25 16:34:30 -0300

    chan

---
+++ contacto.js
@@ -0,0 +1,42 @@
+const nombre = document.getElementById("name")
+const apellido = document.getElementById("apellido")
+const pais = document.getElementById("pais")
+const telefono = document.getElementById("phone")
+const email = document.getElementById("email")
+const parrafo = document.getElementById("warnings")
+
+function validacion(){
+   
+    let warnings = ""
+    parrafo.innerHTML = ""
+        
+    if(nombre.value.length == 0){
+            
+        warnings += " // Ingrese su nombre // "
+        entrar = true
+    }
+    if(apellido.value.length == 0){
+        warnings += " // Ingrese su apellido // "
+        entrar = true
+    }
+    if(pais.value.length == 0){
+        warnings += " // Ingrese su país // "
+        entrar = true
+    }
+    if(telefono.value.length == 0){
+        warnings += " // Ingrese su número de teléfono // "
+        entrar = true
+    }
+
+        /*    if(!regexEmail.test(email.value)){
+            warnings += " // El email no es válido // "
+            entrar = true
+        } */
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado. Recibirá por email sus datos de ingreso"
+    }
+    return false;
+}
+
