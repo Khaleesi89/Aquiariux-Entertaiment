849cde9acc4e1fde0b3068e6d399ab72af0c8297
Khaleesi89 <93682145+Khaleesi89@users.noreply.github.com>
2022-06-26 14:37:04 -0300

    cambios

--- ingresoUsuario.js
+++ ingresoUsuario.js
@@ -1,24 +1,3 @@
-document.addEventListener('DOMContentLoaded',()=>{
-
-    var overlay = document.getElementById("overlay")
-    var popup = document.getElementById("popup")
-    var btnCerrarPopup = document.getElementById("btn-cerrar-popup");
-
-    document.addEventListener('DOMContentLoaded', function(){
-	    overlay.classList.add('active');
-	    popup.classList.add('active');
-    });
-
-    btnCerrarPopup.addEventListener('click', function(e){
-	    e.preventDefault();
-	    overlay.classList.remove('active');
-	    popup.classList.remove('active');
-    });
-
-
-
-
-
 
 const usuario = document.getElementById("usuario")
 const password = document.getElementById("password")
