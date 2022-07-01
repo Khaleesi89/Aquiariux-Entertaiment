93ddc078ba2242ecc04ae7a89536bdd47ea3cadb
Khaleesi89 <93682145+Khaleesi89@users.noreply.github.com>
2022-07-01 10:22:51 -0300

    cmabios

--- escuelas.js
+++ escuelas.js
@@ -1,4 +1,62 @@
+const formulario = document.getElementById('formulario');
+//seleccionamos todos los input de formulario (eso es querySelectorAll)
+const inputs = document.querySelectorAll('#formulario input');
 
+//todas las expresiones regulares. objeto expresiones con propiedades
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+//funcion tipo flecha
+const validarFormulario = (e) =>{
+    //accedemosa los eventos qsuceden en el name q puse dentro de input
+    switch(e.target.name){
+        //pongo las opciones en caso que el name del input sea.... y se ejecuta el codigo q corresponde
+        case "usuario":
+            //comprobamos con los valores q tenemos en elobjeto expresiones usuario.si es verdad devuelve true sino false
+            //hace el test de comprobacion entre lo q hay en dentro del input y lo q yo puse en expresiones regulares
+            if(expresiones.usuario.test(e.target.value)){
+
+            }else{
+                document.getElementById("grupo__usuario")//HACER CAMBIOS EN FORMULARIOS QUEDE EN EL VIDEO EN MIN 18:18
+            }
+        break
+        case "nombre":
+            if(expresiones.nombre.test(e.target.value))
+        break
+        case "apellido":
+            if(expresiones.apellido.test(e.target.value))
+        break
+        case "password":
+            if(expresiones.password.test(e.target.value))
+        break
+        case "email":
+            if(expresiones.email.test(e.target.value))
+        break
+        case "phone":
+            if(expresiones.phone.test(e.target.value))
+        break
+    }
+}
+
+
+//por cada input me ejecutes un codigo
+inputs.forEach((input) =>{
+    //por cada input quiero agregarle un evento cada vez q presiona una tecla va a comprobar los campos
+    input.addEventListener("keyup", validarFormulario);
+    //tambien comprueba cuando hacen click afuera del formulario
+    input.addEventListener("blur", validarFormulario)
+    })
+
+formulario.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+})
+
+/*
 const nombre = document.getElementById("name")
 const apellido = document.getElementById("apellido")
 const documento = document.getElementById("dni")
@@ -65,4 +123,4 @@ function validacion(){
     return false;
 }
 
-
+*/
--- js/app.js
+++ js/app.js
@@ -129,5 +129,3 @@ function vaciarCarrito(){
     };
     articulosCarrito = [];
 };
-
-
